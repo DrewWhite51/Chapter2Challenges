31a58361bdf56086b8d1bfc1235399d14157e3b7
Drew White <drew.white@gus.pittstate.edu>
2020-09-05 12:12:46 -0500

    Added 3rd challenge.

--- src/main/java/PersonalInformation.java
+++ src/main/java/PersonalInformation.java
@@ -11,7 +11,17 @@
 public class PersonalInformation {
     
     public static void main(String[] args) {
+        String yourName = "Drew";
+        String yourAddress = "Mars";
+        String yourState = "Kansas";
+        String yourCity = "Pittsburg";
+        String yourZip = "676767";
+        String yourNumber = "620-687-0244";
+        String yourMajor = "Computer information systems";
         
+        
+        System.out.println(yourName + "\n" + yourAddress + "\n" + yourAddress + "\n" + yourState +
+                 "\n" + yourCity + "\n" + yourZip + "\n" + yourNumber + "\n" + yourMajor);
     }
     
 }
