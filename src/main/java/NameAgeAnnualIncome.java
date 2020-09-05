ce205d266644c5e363b06593724511a4cf97fda0
Drew White <drew.white@gus.pittstate.edu>
2020-09-05 11:49:00 -0500

    Added 1st challenge.

---
+++ src/main/java/NameAgeAnnualIncome.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Drew
+ */
+public class NameAgeAnnualIncome {
+        public static void main(String[] args) {
+        
+        String myName = "Drew White";
+        int myAge = 21;
+        double myAnnualIncome = 100000000;
+        
+        System.out.println("My name is " + myName +
+                 ", I am " + myAge + "\n I hope to make $" + myAnnualIncome +
+                " a year.");
+    }
+}
