b1fe97a311baef9e0c66f78845810c947704f6c7
Drew White <drew.white@gus.pittstate.edu>
2020-09-05 11:56:21 -0500

    Added 2nd challenge.

---
+++ src/main/java/NameAndInitials.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Drew
+ */
+public class NameAndInitials {
+    
+    public static void main(String[] args) {
+        String firstName = "Drew";
+        String lastName = "White";
+        String middleName = "Michael";
+        
+        String  firstInitial = "D";
+        String lastInitial = "W";
+        String middleInitial = "M";
+        
+        
+        System.out.println("My name is " + firstName + " " + middleName + " " + lastName + ".");
+        System.out.println("My initials are " + firstInitial + middleInitial + lastInitial);
+
+    }
+    
+}
