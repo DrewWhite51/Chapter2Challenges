a6ecbd387e182567bedc7be539306fe0abf60c84
Drew White <drew.white@gus.pittstate.edu>
2020-09-06 21:46:09 -0500

    Updates.

---
+++ src/main/java/IngredientAdjuster.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Drew
+ */
+
+import java.util.Scanner;
+public class IngredientAdjuster {
+    
+    public static void main(String[] args) {
+        
+        Scanner keyboard = new Scanner(System.in);
+
+        
+        double oneBatchOfSugar = 1.5;
+        double oneBatchOfButter = 1;
+        double OneBatchOfFlower = 2.75;
+        
+        
+        System.out.println("How many cookies do you want to make?");
+        double cookieAmounts = keyboard.nextInt();
+        
+        
+
+        
+        
+    }
+    
+}
